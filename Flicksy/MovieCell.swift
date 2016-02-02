bfc2e76a4bf51f3e22895503ec7940c376761b32
myulz <myles1@umbc.edu>
2016-02-01 22:11:08 -0500

    first

---
+++ Flicksy/MovieCell.swift
@@ -0,0 +1,30 @@
+//
+//  MovieCell.swift
+//  Flicksy
+//
+//  Created by Myles Johnson on 2/1/16.
+//  Copyright © 2016 Myles. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var overviewLabel: UILabel!
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Pods/Target Support Files/Pods-Flicksy/Pods-Flicksy-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_FlicksyVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_FlicksyVersionString[];
+
